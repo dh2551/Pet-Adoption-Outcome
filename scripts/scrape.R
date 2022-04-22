1cbfe75fbbb1b9c218c729b8c77d897c81da3652
dh2551 <56976631+dh2551@users.noreply.github.com>
2022-04-22 14:41:18 -0400

    api pull code

--- scripts/scrape.R
+++ scripts/scrape.R
@@ -5,12 +5,16 @@ library(jsonlite)
 
 #try curl 
 api_token <- ''
-raw_data <- httr::GET(url,accept_json(), add_headers("Authorization" = paste("Bearer", api_token, sep = " ")))
 
 
-mydata <- httr::content(raw_data,type="application/json")
-mydata<- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
+animals_data_adopted <- foreach(page = 1:100, .combine='rbind') %do% {
+  urls <- paste0('https://api.petfinder.com/v2/animals/?status=adopted&page=',page,'&limit=100')
+  raw_data <- httr::GET(urls,accept_json(), add_headers("Authorization" = paste("Bearer", api_token, sep = " ")))
+  mydata <- httr::content(raw_data,type="application/json")
+  mydata <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
+  animals <- mydata$animals
+} 
 
-animals <- mydata$animals
+
 
 
