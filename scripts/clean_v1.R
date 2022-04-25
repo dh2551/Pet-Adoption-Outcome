eedbc6d65cd4604968eab379330f52dc45c56f0d
dh2551 <56976631+dh2551@users.noreply.github.com>
2022-04-25 19:45:42 -0400

    lets try again lol

--- scripts/clean_v1.R
+++ scripts/clean_v1.R
@@ -12,37 +12,50 @@
 ### load library 
 library(tidyverse)
 library(lubridate)
+library(tidytext)
 
 
 ### load data 
 
-adopted_all_pre_2020 <- read_csv('./data/adopted_before_2020_76000.csv' )
+cat_pre_2020 <- read_csv('./data/cat_before_2020.csv' )
+dog_pre_2020 <- read_csv('./data/dog_adopted_before_2020.csv' )
+
+adopted_all_pre_2020 <- rbind(cat_pre_2020, dog_pre_2020)
 
 ## check na rate 
 empty_rate <- adopted_all_pre_2020 %>% summarise_all(list(name = ~sum(is.na(.))/length(.)))
 
 ## get cats and dogs and drop columns with too many NAs 
 ## unsure if we should drop primary color
-adopted_pre_2020_v1 <- adopted_all_pre_2020 %>%
+adopted_pre_2020_full <- adopted_all_pre_2020 %>%
   filter(status_changed_at != published_at,
-         type == 'Cat' | type == 'Dog',
          type == species) %>%
   select(-coat, -tags,-organization_animal_id, -photos, -videos, -distance, 
          -breeds.secondary, -colors.secondary, -colors.tertiary,-attributes.declawed, # Just changed the order features
          -primary_photo_cropped.small, -primary_photo_cropped.medium,       ##not sure about dropping these
          -primary_photo_cropped.large, -primary_photo_cropped.full,         ##we should drop them
-         -contact.address.address1, -contact.address.address2, -contact.address.postcode,
-         -url,-species,-`_links.self.href`, -`_links.type.href`, -`_links.organization.href`,
-         -name,-colors.primary, -country) # i don't think name matters; there are 14999 NAs for color, and I still not sure if we keep it; there are only 1822 for CA, I think we drop it
+         -contact.address.address1, -contact.address.address2, -contact.address.postcode) 
 
-#check again
-empty_rate_2 <- adopted_pre_2020_v1 %>% summarise_all(list(name = ~sum(is.na(.))/length(.)))
+# i don't think name matters; there are 14999 NAs for color, and I still not sure if we keep it;
+# there are only 1822 for CA, I think we drop it
+
+# check again
+empty_rate_2 <- adopted_pre_2020_full %>% summarise_all(list(name = ~sum(is.na(.))/length(.)))
+
+
+# drop columns that are useless 
+adopted_pre_2020_v1 <- adopted_pre_2020_full %>%
+  select(-url, -species, -`_links.self.href`, -`_links.type.href`, -`_links.organization.href`,
+         -name, -colors.primary, -contact.address.country)
+
+
 # re-level columns 
 #   age: baby 1, young 2, adult 3, senior 4 (as.factor)
 #   size: small 1, medium 2, large 3, extra large 4 (as.factor)
 #   gender: male 1, female 2 (as.factor)
 #   environment.child/dog/cat: true 1, false 0, na--> uncertain 2 (as.factor)
-#   
+
+ 
 # construct new columns
 #   adopted within a month: yes 1, no 0
 #   description: should we do a word count column + number of positive word used to describe pet column
@@ -51,7 +64,6 @@ empty_rate_2 <- adopted_pre_2020_v1 %>% summarise_all(list(name = ~sum(is.na(.))
 #                    list of most popular, and top 5 label 1-5, others 0?
 #   contact: email, phone, both 
 #   
-# 
 
 adopted_pre_2020_v1 <- adopted_pre_2020_v1 %>%
   mutate(duration_adopt = difftime(status_changed_at,published_at, unit="days"),
@@ -59,7 +71,17 @@ adopted_pre_2020_v1 <- adopted_pre_2020_v1 %>%
          contacts = case_when(is.na(contact.phone) == TRUE & is.na(contact.email) == FALSE ~ 'email',
                                   is.na(contact.phone) == FALSE & is.na(contact.email) == TRUE ~ 'phone',
                                   is.na(contact.phone) == FALSE & is.na(contact.email) == FALSE ~'both',
-                                  TRUE ~ 'neither'))
+                                  TRUE ~ 'neither') )
+
+desc_length <- adopted_pre_2020_v1 %>%
+  mutate(descript = ifelse(is.na(description), '', description)) %>%
+  select(id, descript) %>%
+  unnest_tokens(word, descript) 
+
+word_count <- desc_length %>%
+  group_by(id) %>%
+  mutate(ct = ifelse())
+  count()
 
 
 
