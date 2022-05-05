43b7f8ec2ab3ac6ee238271da40e69919b64f092
dh2551 <56976631+dh2551@users.noreply.github.com>
2022-05-05 12:27:29 -0400

    final final

--- scripts/scrape.R
+++ scripts/scrape.R
@@ -7,7 +7,7 @@ library(jsonlite)
 api_token <- '' ## insert your token 
 
 dog_adopted_pre_2020 <- foreach(page = 1:1000, .combine='bind_rows') %do% {
-  urls <- paste0('https://api.petfinder.com/v2/animals?type=dog&status=adopted&before=2020-01-01T00:00:00-05:00&page=',page,'&limit=100') #status=adopted&
+  urls <- paste0('https://api.petfinder.com/v2/animals?type=Dog&status=adopted&before=2020-01-01T00:00:00-05:00&page=',page,'&limit=100') #status=adopted&
   raw_data <- httr::GET(urls,accept_json(), add_headers("Authorization" = paste("Bearer", api_token, sep = " ")))
   mydata <- httr::content(raw_data,type="application/json")
   mydata <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
@@ -15,7 +15,7 @@ dog_adopted_pre_2020 <- foreach(page = 1:1000, .combine='bind_rows') %do% {
 } 
 
 cat_adopted_pre_2020 <- foreach(page = 1:1000, .combine='bind_rows') %do% {
-  urls <- paste0('https://api.petfinder.com/v2/animals?type=cat&status=adopted&before=2020-01-01T00:00:00-05:00&page=',page,'&limit=100') #status=adopted&
+  urls <- paste0('https://api.petfinder.com/v2/animals?type=Cat&status=adopted&before=2020-01-01T00:00:00-05:00&page=',page,'&limit=100') #status=adopted&
   raw_data <- httr::GET(urls,accept_json(), add_headers("Authorization" = paste("Bearer", api_token, sep = " ")))
   mydata <- httr::content(raw_data,type="application/json")
   mydata <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
@@ -23,7 +23,7 @@ cat_adopted_pre_2020 <- foreach(page = 1:1000, .combine='bind_rows') %do% {
 }
 
 dog_adopted_pre_2021 <- foreach(page = 1:1000, .combine='bind_rows') %do% {
-  urls <- paste0('https://api.petfinder.com/v2/animals?type=dog&status=adopted&before=2021-01-01T00:00:00-05:00&page=',page,'&limit=100') #status=adopted&
+  urls <- paste0('https://api.petfinder.com/v2/animals?type=Dog&status=adopted&before=2021-01-01T00:00:00-05:00&page=',page,'&limit=100') #status=adopted&
   raw_data <- httr::GET(urls,accept_json(), add_headers("Authorization" = paste("Bearer", api_token, sep = " ")))
   mydata <- httr::content(raw_data,type="application/json")
   mydata <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
@@ -31,33 +31,49 @@ dog_adopted_pre_2021 <- foreach(page = 1:1000, .combine='bind_rows') %do% {
 } 
 
 cat_adopted_pre_2021 <- foreach(page = 1:1000, .combine='bind_rows') %do% {
-  urls <- paste0('https://api.petfinder.com/v2/animals?type=cat&status=adopted&before=2021-01-01T00:00:00-05:00&page=',page,'&limit=100') #status=adopted&
+  urls <- paste0('https://api.petfinder.com/v2/animals?type=Cat&status=adopted&before=2021-01-01T00:00:00-05:00&page=',page,'&limit=100') #status=adopted&
   raw_data <- httr::GET(urls,accept_json(), add_headers("Authorization" = paste("Bearer", api_token, sep = " ")))
   mydata <- httr::content(raw_data,type="application/json")
   mydata <- fromJSON(rawToChar(raw_data$content), flatten = TRUE)
   animals <- mydata$animals
 } 
 
-dog_2020_q4 <- dog_adopted_pre_2021 %>% 
-  filter(published_at < '2021-01-01T00:00:00-05:00' & published_at > '2020-10-01T00:00:00-05:00')
 
-dog_2019_q4 <- dog_adopted_pre_2020 %>% 
-  filter(published_at < '2020-01-01T00:00:00-05:00' & published_at > '2019-10-01T00:00:00-05:00')
+write_csv(cat_adopted_pre_2020, './data/cat_2019.csv')
 
-cat_2020_q4 <- cat_adopted_pre_2021 %>% 
-  filter(published_at < '2021-01-01T00:00:00-05:00' & published_at > '2020-10-01T00:00:00-05:00')
+write_csv(dog_adopted_pre_2020, './data/dog_2019.csv')
 
-cat_2019_q4 <- cat_adopted_pre_2020 %>% 
-  filter(published_at < '2020-01-01T00:00:00-05:00' & published_at > '2019-10-01T00:00:00-05:00')
+write_csv(cat_adopted_pre_2021, './data/cat_2020.csv')
+
+write_csv(dog_adopted_pre_2021, './data/dog_2020.csv')
 
 
-write_csv(cat_adopted_pre_2020 , './data/cat_2019_q4.csv')
 
-write_csv(cat_adopted_pre_2021 , './data/cat_2020_q4.csv')
+dog_2020 <- read_csv('./data/dog_2020.csv')
+dog_2019 <- read_csv('./data/dog_2019.csv')
+cat_2020 <- read_csv('./data/cat_2020.csv')
+cat_2019 <- read_csv('./data/cat_2019.csv')
 
-write_csv(cat_adopted_pre_2020 , './data/cat_2019_q4.csv')
+dog_2020_q4 <- dog_2020 %>% 
+  filter(published_at <= ymd('2021-01-01')& published_at >= ymd('2020-10-01'))
 
-write_csv(dog_adopted_pre_2021 , './data/dog_2020_q4.csv')
+dog_2019_q4 <- dog_2019 %>% 
+  filter(published_at <= ymd('2020-01-01')& published_at >= ymd('2019-10-01'))
+
+cat_2020_q4 <- cat_2020 %>% 
+  filter(published_at <= ymd('2021-01-01')& published_at >= ymd('2020-10-01'))
+
+cat_2019_q4 <- cat_2019 %>% 
+  filter(published_at <= ymd('2020-01-01')& published_at >= ymd('2019-10-01'))
+
+
+write_csv(cat_2019_q4, './data/cat_2019_q4.csv')
+
+write_csv(dog_2019_q4, './data/dog_2019_q4.csv')
+
+write_csv(cat_2020_q4, './data/cat_2020_q4.csv')
+
+write_csv(dog_2020_q4, './data/dog_2020_q4.csv')
 
 
 
